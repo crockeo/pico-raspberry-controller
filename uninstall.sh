1fcb9a62dc0e98dfabb5ebd8831658961ff0f5f4
Cerek Hillen <cerekh@gmail.com>
2019-12-02 23:40:20 -0500

    Debugging on Raspberry Pi

--- uninstall.sh
+++ uninstall.sh
@@ -8,5 +8,6 @@ then
 fi
 
 # Disabling and removing the service file
+$SUDO systemctl stop pico-raspberry
 $SUDO systemctl disable pico-raspberry
-$SUDO rm /etc/systemd/system/pico-raspberry.system
+$SUDO rm /etc/systemd/system/pico-raspberry.service
